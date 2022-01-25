ed2b3a6a97742a2c620806f15d1aede8f7812d62
pojh5090 <pojh4@naver.com>
2022-01-25 17:35:34 +0900

    input 전까지

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 /* eslint-disabled */
 import React, { useState } from 'react';  //react의 내장함수를 쓸게요~
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
+
+  function 반복된UI() {
+    var 어레이 = [];
+    for(var i = 0; i < 3; i++) {
+      어레이.push(<div>안녕</div>);
+    }
+    return 어레이;
+  }
 
   // function 제목바꾸기() {
   //   var newArray = [...글제목];
@@ -22,31 +30,33 @@ function App() {
         <div>개발 Blog</div>
       </div>
       {
-        글제목.map(function(글){
+        글제목.map(function(글, i){
           return (
           <div className='list'>
-            <h3>{글} <span style={ {cursor : 'pointer'} } onClick={() => { 따봉변경(따봉 + 1) } }>👍</span>{ 따봉 }</h3>
+            <h3 onClick = {() => { 누른제목변경(i) }}>{글} <span style={ {cursor : 'pointer'} } onClick={() => { 따봉변경(따봉 + 1) } }>👍</span>{ 따봉 }</h3>
             <p>2월 15일 발행</p>
             <hr/>
           </div>
           )
         })
       }
+      {/* {반복된UI()} */}
+      <input />
 
-      <button style={{ cursor : 'pointer'}} onClick={ () => { modal변경(!modal)}}>열고 닫기</button>
+      <button style={{ cursor : 'pointer'}} onClick={ () => { modal변경(!modal) }}>열고 닫기</button>
       {
         modal === true
-        ? <Modal />
+        ? <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
         : null
       }
     </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return (
     <div className="modal">
-        <h2>제목</h2>
+        <h2>{ props.글제목[props.누른제목]}</h2>
         <p>날짜</p>
         <p>상세내용</p>
     </div>
