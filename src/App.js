a92d53487dbf741d50492863b53547e9a3953cb0
pojh5090 <kjh0611@mobigen.com>
2022-01-25 14:13:30 +0900

    0125

--- src/App.js
+++ src/App.js
@@ -1,48 +1,44 @@
 /* eslint-disabled */
-
 import React, { useState } from 'react';  //react의 내장함수를 쓸게요~
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동집', '퇴근하고싶다..', '졸려요']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
-  let posts = '강남 고기 맛집';
+  let [modal, modal변경] = useState(false);
 
-  function 제목바꾸기() {
-    var newArray = [...글제목];
-    newArray[0] = '여자 코트 추천';
-    글제목변경(newArray);
+  // function 제목바꾸기() {
+  //   var newArray = [...글제목];
+  //   newArray[0] = '여자 코트 추천';
+  //   글제목변경(newArray);
+  // }
+  //==> 글제목변경(['여자 코트 추천', '강남 우동집', '퇴근하고싶다..', '졸려요']); 와 같음!
+  //<button style={{ cursor : 'pointer'}} onClick={제목바꾸기}>버튼</button>
 
-  //글제목변경(['여자 코트 추천', '강남 우동집', '퇴근하고싶다..', '졸려요']);
-  }
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <div className="list">
-        <button onClick={제목바꾸기}>버튼</button>
-        <h3>{ 글제목[0] } <span style={ {cursor : 'pointer'} } onClick={() => { 따봉변경(따봉 + 1) } }>👍</span>{ 따봉 }</h3>
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className='list'>
-        <h3>{ 글제목[1] }</h3>
-        <p>2월 16일 발행</p>
-        <hr/>
-      </div>
-      <div className='list'>
-        <h3>{ 글제목[2] }</h3>
-        <p>2월 15일 발행</p>
-        <hr/>
-      </div>
-      <div className='list'>
-        <h3>{ 글제목[3] }</h3>
-        <p>2월 14일 발행</p>
-        <hr/>
-      </div>
-      <Modal></Modal>
+      {
+        글제목.map(function(글){
+          return (
+          <div className='list'>
+            <h3>{글} <span style={ {cursor : 'pointer'} } onClick={() => { 따봉변경(따봉 + 1) } }>👍</span>{ 따봉 }</h3>
+            <p>2월 15일 발행</p>
+            <hr/>
+          </div>
+          )
+        })
+      }
+
+      <button style={{ cursor : 'pointer'}} onClick={ () => { modal변경(!modal)}}>열고 닫기</button>
+      {
+        modal === true
+        ? <Modal />
+        : null
+      }
     </div>
   );
 }
